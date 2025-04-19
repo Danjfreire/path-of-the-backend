bb654304b48caa81ab678c89e4239e65325b0f91
Daniel Freire <danjfreire@gmail.com>
2025-04-19 14:53:05 -0300

    feat: setup local dev db

---
+++ phase-1/01-the-basic-todo-api/src/v1/auth/auth.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller('v1/auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post()
+  async registerUser() {}
+
+  @Post('login')
+  async loginUser() {}
+}
