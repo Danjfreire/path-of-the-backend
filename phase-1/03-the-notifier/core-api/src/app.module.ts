9ad13484a4aa05ad873c97d35b9051bc5b9f7abc
danjfreire <danjfreire@gmail.com>
2025-05-18 07:53:17 -0300

    chore: remove unused files

--- phase-1/03-the-notifier/core-api/src/app.module.ts
+++ phase-1/03-the-notifier/core-api/src/app.module.ts
@@ -1,24 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ClientsModule, Transport } from '@nestjs/microservices';
 import { V1Module } from './v1/v1.module';
 
 @Module({
-  imports: [
-    V1Module,
-    ClientsModule.register([
-      {
-        name: 'NOTIFICATION_SERVICE',
-        transport: Transport.RMQ,
-        options: {
-          urls: ['amqp://guest:guest@rabbitmq:5672'],
-          queue: 'notifications',
-        },
-      },
-    ]),
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [V1Module],
 })
 export class AppModule {}
